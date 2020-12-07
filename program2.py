2ecda1a4b3ca1e1ec89ccc1ed475b2e6f675bd03
Megasteel32 <megasteel32@gmail.com>
2020-12-06 18:57:19 -0600

    shit

--- program2.py
+++ program2.py
@@ -1,3 +1,12 @@
+# By submitting this assignment, I agree to the following:
+#  Aggies do not lie, cheat, or steal, or tolerate those who do
+#  I have not given or received any unauthorized aid on this assignment
+#
+# Name:         Luca Maddaleni, 330001030
+# Section:      273
+# Assignment:   Lab 13b Assignment 2
+# Date:         12/6/2020
+
 from numpy import *
 from matplotlib.pyplot import *
 
